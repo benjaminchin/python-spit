5913c700996e46356b9ffe62c011ea48f27f0497
Benjamin Chin <benjamin.d.chin@gmail.com>
2022-06-29 18:00:54 -0400

    collision attempt

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     canvas = pg.Surface((WIDTH, HEIGHT))
 
+    all_sprites = pg.sprite.Group()
+
     screen.fill(BLACK)
     canvas.fill(BLACK)
     pg.display.set_caption(TITLE)
@@ -25,6 +27,8 @@ def main():
         deck.append(Card(i+1, 'c'))
    
     random.shuffle(deck)
+    for card in deck:
+        all_sprites.add(card.sprite)
 
     while running:
         for event in pg.event.get():
@@ -38,11 +42,15 @@ def main():
         screen.fill(GREEN)
         canvas.fill(GREEN)
         
-        canvas.blit(deck[0].sprite, (125 - CARD_WIDTH/2, HEIGHT/2 - CARD_HEIGHT/2))
+        canvas.blit(deck[0].sprite, (125 - CARD_WIDTH/2, HEIGHT/2 - CARD_HEIGHT/2)) # slot 1-5
         canvas.blit(deck[1].sprite, (250 - CARD_WIDTH/2, HEIGHT/2 - CARD_HEIGHT/2))
         canvas.blit(deck[2].sprite, (375 - CARD_WIDTH/2, HEIGHT/2 - CARD_HEIGHT/2))
         canvas.blit(deck[3].sprite, (500 - CARD_WIDTH/2, HEIGHT/2 - CARD_HEIGHT/2))
         canvas.blit(deck[4].sprite, (625 - CARD_WIDTH/2, HEIGHT/2 - CARD_HEIGHT/2))
+
+        for card in deck[0:5]:
+            if card.sprite.get_rect().collidepoint(x, y):
+                print('collision!')
 
         screen.blit(canvas, (0, 0))
         pg.display.update()
